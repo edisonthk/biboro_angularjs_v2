50c1a7c630b58cef60c77f20f2dca509c8d37a79
takeokunn <bararararatty@gmail.com>
2015-08-20 22:13:51 +0900

    edit workbook.service

--- src/app/components/workbook/workbook.service.js
+++ src/app/components/workbook/workbook.service.js
@@ -3,9 +3,101 @@ import BaseService from "../../base/base.service";
 class WorkbookService extends BaseService{
     constructor($http,dispatcher) {
         super($http,dispatcher);
-
-        
+        this.WORKBOOK_FETCHEDALL_CALLBACK = "WORKBOOK_FETCHEDALL_CALLBACK";
+        this.WORKBOOK_STORE_CALLBACK = "WORKBOOK_STORE_CALLBACK";
+        this.WORKBOOK_SHOW_CALLBACK = "WORKBOOK_SHOW_CALLBACK";
+        this.WORKBOOK_UPDATE_CALLBACK = "WORKBOOK_UPDATE_CALLBACK";
+        this.WORKBOOK_DESTROY_CALLBACK = "WORKBOOK_DESTROY_CALLBACK";
     }
+    registerFetchedAllCallback(callback) {
+        this._dispatcher.register(this.WORKBOOK_FETCHEDALL_CALLBACK,callback);
+    }
+
+    registerStoreCallback(callback) {
+        this._dispatcher.register(this.WORKBOOK_STORE_CALLBACK,callback);
+    }
+
+    registerShowCallback(callback) {
+        this._dispatcher.register(this.WORKBOOK_SHOW_CALLBACK,callback);
+    }
+
+    registerUpdateCallback(callback) {
+        this._dispatcher.register(this.WORKBOOK_UPDATE_CALLBACK,callback);
+    }
+
+    registerDestroyAllCallback(callback) {
+        this._dispatcher.register(this.WORKBOOK_DESTROY_CALLBACK,callback);
+    }
+
+    fetchAll() {
+        var self = this;
+
+        self._http.get('/api/v1/workbook/index.json')
+            .success(function(data){
+                self.workbooks = data;
+                self._dispatcher.dispatch(self.WORKBOOK_FETCHEDALL_CALLBACK, {"success":true,"result":"success","response":self.getWorkbooks()});
+            })
+            .error(function(){
+                console.log("error in workbook.strore.js");
+                self._dispatcher.dispatch(self.WORKBOOK_FETCHEDALL_CALLBACK, {"success":false,"result":"fail to fetch workbooks."});
+            });
+
+    }
+
+    store(){
+        var self= this;
+
+        self._http.post('/api/v1/workbook/index.json')
+            .success(function(response){
+                self._dispatcher.dispatch(self.WORKBOOK_STORE_CALLBACK, {"success":true,"result":"success", "response": response});
+            })
+            .error(function(){
+                self._dispatcher.dispatch(self.WORKBOOK_STORE_CALLBACK, {"success":false,"result":"fail to store workbooks."});
+            });
+    }
+
+    show(id){
+        var self= this;
+
+        self._http.get('/api/v1/workbook/'+id+'/index.json')
+            .success(function(response){
+                self.workbook = response;
+                self._dispatcher.dispatch(self.WORKBOOK_SHOW_CALLBACK, {"success":true,"result":"success","response":response});
+            })
+            .error(function(){
+                self._dispatcher.dispatch(self.WORKBOOK_SHOW_CALLBACK, {"success":false,"result":"fail to show workbooks."});
+            });
+    }
+
+    update(id){
+        var self= this;
+
+        self._http.put('/api/v1/workbook/'+id+'/index.json')
+            .success(function(response){
+                self._dispatcher.dispatch(self.WORKBOOK_UPDATE_CALLBACK, {"success":true,"result":"success","response":response});
+            })
+            .error(function(){
+                self._dispatcher.dispatch(self.WORKBOOK_UPDATE_CALLBACK, {"success":false,"result":"fail to update workbooks."});
+            });
+    }
+
+    destroy(id){
+        var self= this;
+
+        self._http.delete('/api/v1/workbook/'+id+'/index.json')
+            .success(function(response){
+                self._dispatcher.dispatch(self.WORKBOOK_DESTROY_CALLBACK, {"success":true,"result":"workbook","response":response});
+            })
+            .error(function(){
+                self._dispatcher.dispatch(self.WORKBOOK_DESTROY_CALLBACK, {"success":false,"result":"fail to destroy workbooks."});
+            });
+    }
+
+
+    getWorkbooks(){
+        return this.workbooks;
+    }
+
 }
 
 WorkbookService.$inject = ["$http", "Dispatcher"];
